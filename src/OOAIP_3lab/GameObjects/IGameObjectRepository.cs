b0a05028230fdc78ed8c90ce27ab3166686e8ddb
Vadim1044 <67069732+Vadim1044@users.noreply.github.com>
2026-06-06 18:59:58 +0600

    Delete src/OOAIP_3lab directory

--- src/OOAIP_3lab/GameObjects/IGameObjectRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace OOAIP_3lab.GameObjects;
-
-public interface IGameObjectRepository
-{
-    void Add(IGameObject gameObject);
-    void Remove(IGameObject gameObject);
-    IEnumerable<IGameObject> GetAll();
-    IGameObject GetById(Guid id);
-}
