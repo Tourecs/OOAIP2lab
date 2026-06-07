f82d252457e6ec77d99be727c8a6996e9b2030fb
Vadim1044 <67069732+Vadim1044@users.noreply.github.com>
2026-06-07 22:52:30 +0600

    Add files via upload

---
+++ src/SpaceBattle/Commands/MovingObjectAdapter.cs
@@ -0,0 +1,19 @@
+namespace SpaceBattle;
+
+public sealed class MovingObjectAdapter : IMovingObject
+{
+    private readonly IDictionary<string, object> _data;
+
+    public MovingObjectAdapter(IDictionary<string, object> data)
+    {
+        _data = data;
+    }
+
+    public Vector Position
+    {
+        get => (Vector)_data["Position"];
+        set => _data["Position"] = value;
+    }
+
+    public Vector Velocity => (Vector)_data["Velocity"];
+}
